86b013f390b8152b9f0c9708dd1311b1e1ff7c5a
Adam Perlman <adam@offtrackbetting.com>
2020-06-13 22:14:55 +0000

    SessionRefresh: call full url now

--- js/sessionrefresh.js
+++ js/sessionrefresh.js
@@ -1,5 +1,14 @@
 // When logged in we need to ensure that the session is refreshed
+/*
 $.get('/json/sessionrefresh');
 setInterval(function() {
 	$.get('/json/sessionrefresh');
 }, 1000 * 60 * 15); // 15 minutes
+*/
+
+$.get('https://bet.offtrackbetting.com/api/sessionrefresh');
+setInterval(function() {
+	$.get('https://bet.offtrackbetting.com/api/sessionrefresh');
+}, 1000 * 60 * 15); // 15 minutes
+
+
