969a161adc97ce01292907ed1a98f8cf9fc8418b
Adam Perlman <adam@offtrackbetting.com>
2022-03-24 12:49:32 +0000

    Schedule v2 JS Page

--- js/schedule.js
+++ js/schedule.js
@@ -1,45 +1,75 @@
 /*
-Schedule - populate from cloudfront
-*/
-// const TodaysSchedule = (scheduleData) => {
-//   return scheduleData.todaysraces.tracks.map((track) => `
-//     <tr>
-//       <td><a href="${track.resultsUrl}">${track.name}</a></td>
-//       <td>${track.time}</td>
-//     </tr>
-//   `).join("\n");
-// };
+ Schedule - populate from cloudfront
 
-const TodaysSchedule = (scheduleData) => 
-  scheduleData.todaysraces.tracks.map((track) => `
+ - TodaysSchedule: given scheduleData, return rows of table
+ - FutureSchedules: given dates (YYYY-mm-dd), populate table for each date
+   - ThisDaysSchedule(scheduleData): return individual day's schedule rows
+*/
+
+
+function ConvertTime(date, timeStr) {
+  // ETOffset --> set in main page
+  const offset = ETOffset + (new Date().getTimezoneOffset())/60;
+  const timeParts = timeStr.split(/:| /).map(
+    (part, idx) => (idx < 2) ? parseInt(part) : part);
+  const ampm = timeParts.pop();
+  timeParts[0] += (ampm == "PM") ? 12 : 0;
+
+  const dateParts = date.split('-').map((d) => parseInt(d));
+  dateParts[1] -= 1;
+
+  const localTime = new Date(...[...dateParts, ...timeParts, 0]);
+  localTime.setHours(localTime.getHours() - offset);
+
+  return localTime.toLocaleTimeString().replace(':00', '') + " (local time)";
+}
+
+function DisplayLocalTime(date, cell) {
+  current_value = cell.innerHTML;
+  return (original) => cell.innerHTML = (original) ? current_value : 
+    ConvertTime(date, current_value); 
+}
+
+function ThisDaysSchedule(date, scheduleData) {
+  return scheduleData.sort((a, b) => {
+    if (a.name > b.name)
+      return 1;
+    if (a.name < b.name)
+      return -1;
+    return 0;
+  }).map((track) => `
+      <tr>
+        <td>${track.canceled ? "<s>" : ""}${track.name}${
+          track.canceled ? "</s>" : ""}</td>
+        <td onMouseOver="localTime=DisplayLocalTime('${date}', this); 
+          localTime(false);"
+          onMouseOut="localTime(true);"
+          >${track.posttime}</td>
+      </tr>`);
+}
+
+function FutureSchedules(dates) {
+  dates.forEach((date) => {
+    // YYYY-mm-dd --> YYYY/mmdd
+    const dateURL = date.replace('-','/').replace('-','');
+    $.getJSON({
+      url: `https://json.offtrackbetting.com/schedule/${dateURL}.json`,
+      crossDomain: true,
+      success: (scheduleData) => $(`#day-${date} tbody`).html(
+        ThisDaysSchedule(date, scheduleData)),
+      error: (err) => {
+        console.log("fail");
+        console.log(err);
+      }
+    });
+  });
+}
+
+function TodaysSchedule(date, scheduleData) {
+  return scheduleData.todaysraces.tracks.map((track) => `
     <tr>
       <td><a href="${track.resultsUrl}">${track.name}</a></td>
-      <td>${track.time}</td>
-    </tr>
-  `).join("\n");
-
-const ThisDaysSchedule = (scheduleData) =>
-  scheduleData.sort((a, b) => {
-    if (a.name > b.name) return 1;
-    if (a.name < b.name) return -1;
-    return 0 }).map((track) => `
-    <tr>
-      <td>${track.canceled ? "<s>" : ""}
-        ${track.name}
-        ${track.canceled ? "</s>" : ""}</td>
-      <td>${track.posttime}</td>
-    </tr>
-  `);
-
-const FutureSchedules = (dates) => {
-  dates.forEach((date) => $.getJSON({
-    url: "https://json.offtrackbetting.com/schedule/2022/0322.json",
-    crossDomain: true,
-    success: (scheduleData) => $(`#day-${date} tbody`).append(
-      ThisDaysSchedule(scheduleData)),
-    error: (err) => {
-      console.log("fail");
-      console.log(err);
-    }
-  }));
-};
+      <td onMouseOver="localTime=DisplayLocalTime('${date}', this); 
+      localTime(false);" onMouseOut="localTime(true);">${track.time}</td>
+    </tr>`).join("\n");
+}
